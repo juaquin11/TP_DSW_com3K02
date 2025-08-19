721b758446cd7b622b1c4a6c9cd43c0150611551
tomy19012005 <tomylevrand@gmail.com>
2025-08-19 15:41:58 -0300

    get restaurant frontend + login/register

--- frontend/src/services/restaurantService.ts
+++ frontend/src/services/restaurantService.ts
@@ -3,8 +3,6 @@ import type { RestaurantDTO } from '../types/restaurant';
 
 const API_BASE = 'http://localhost:3000/api';
 
-
-
 export async function fetchRestaurants(): Promise<RestaurantDTO[]> {
   const res = await axios.get<RestaurantDTO[]>(`${API_BASE}/restaurants`);
   return res.data;
