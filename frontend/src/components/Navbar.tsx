476116349265e173be6103b63b4bac4b3e5a38c5
tomy19012005 <tomylevrand@gmail.com>
2025-08-20 11:32:23 -0300

    Implementacion de CSS Modules y cambio en Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css"; // vamos a crear este archivo
+import styles from "./Navbar.module.css"; // import con alias "styles"
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
         <Link to="/">🍽️ FoodApp</Link>
       </div>
-      <ul className="navbar__links">
+      <ul className={styles.links}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">Register</Link></li>
