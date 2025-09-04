d15f2dd1b417fdf1bd2a48531be45769257d2031
Joaco <joaaperalta@gmail.com>
2025-09-03 21:30:37 -0300

    Cambios en el front

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,19 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css"; // import con alias "styles"
 import Logo from "./logo";
+import { useNavigate } from "react-router-dom";
+
 
 
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    // podés hacer cosas antes, ej: limpiar estados, logs, etc.
+    navigate("/login");
+  };
+
+  const handleRegisterClick = () => {
+    // podés hacer cosas antes, ej: limpiar estados, logs, etc.
+    navigate("/register");
+  };
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Link to="/">{<Logo width="3rem" height="3rem"></Logo>} FoodApp</Link>
+      <div className={styles.container}>
+        <div className={styles.topNav}>
+          <div className={styles.logo}>
+            <Link to="/">{<Logo width="3rem" height="3rem"></Logo>}</Link>
+            <h1><Link to="/">FoodApp</Link></h1>
+          </div>
+
+          <div className={styles.actions}>
+            <button onClick={handleLoginClick} className={`${styles.loginButton} ${styles.loginButtonVisible}`}>Login</button>
+            <button onClick={handleRegisterClick} className={`${styles.registerButton} ${styles.registerButtonVisible}`}>Register</button>
+          </div>
+        </div>
       </div>
-      <ul className={styles.links}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-      </ul>
     </nav>
   );
 };
